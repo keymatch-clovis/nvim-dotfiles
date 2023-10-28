b1ba5ae0485c777f66f8d3a1c4a640f9645ee297
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2023-10-27 21:26:14 -0500

    feat: Added conform.nvim as formatter.

--- init.lua
+++ init.lua
@@ -474,9 +474,10 @@ local on_attach = function(_, bufnr)
   end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+  -- Using conform.nvim
+  --vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+  --vim.lsp.buf.format()
+  --end, { desc = 'Format current buffer with LSP' })
 end
 
 -- document existing key chains
@@ -620,5 +621,3 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- This is going to get me cancelled, way comfy
 vim.keymap.set("i", "<C-c>", "<Esc>")
-
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "LSP Buffer [F]ormat" })
