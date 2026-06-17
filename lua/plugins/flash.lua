d7ac276c76e0cf0e274dde0591c43053ef85f49b
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2026-06-17 11:24:06 -0500

    feat: adds flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,3 +1,10 @@
 vim.pack.add { "https://github.com/folke/flash.nvim" }
 
 require('flash').setup()
+
+vim.keymap.set(
+    'n',
+    'gs',
+    function() require('flash').jump() end,
+    { desc = 'Flash' }
+)
